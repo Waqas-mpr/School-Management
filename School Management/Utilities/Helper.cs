14b6239b67c478bd4b8dc726aa7369e2f3e215fa
Waqas Ahmed <Waqas-mpr@users.noreply.github.com>
2024-09-15 19:17:35 +0500

    Done

--- School Management/Utilities/Helper.cs
+++ School Management/Utilities/Helper.cs
@@ -11,8 +11,13 @@ namespace School_Management.Utilities
         private static List<Student> _studentDataStore = DataStore.GetInstance.StudentsData();
         public static int GenerateId()
         {
-            int maxStudentId = _studentDataStore.Max(s => s.Id);
-            int maxTeacherId = _teacherDataStore.Max(t => t.Id);
+            int maxStudentId;
+            int maxTeacherId;
+
+            maxStudentId= _studentDataStore.Count != 0? _studentDataStore.Max(s => s.Id) : 0;
+
+            maxTeacherId= _teacherDataStore.Count != 0 ? _teacherDataStore.Max(t => t.Id) : 0;
+       
 
             if(maxStudentId > maxTeacherId)
                 return maxStudentId +1;
